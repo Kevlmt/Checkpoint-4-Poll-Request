23c9fb30885a08bdf6d48710d06d7b57f89bf1c2
Kevlmt <97613786+Kevlmt@users.noreply.github.com>
2023-01-12 16:59:15 +0100

    change category manager

--- backend/src/models/CategoriesManager.js
+++ backend/src/models/CategoriesManager.js
@@ -6,6 +6,13 @@ class CategoriesManager extends AbstractManager {
   findAll() {
     return this.connection.query(`SELECT * FROM ${CategoriesManager.table}`);
   }
+
+  createOne(category) {
+    return this.connection.query(
+      `INSERT INTO ${CategoriesManager.table} SET ?`,
+      category
+    );
+  }
 }
 
 module.exports = CategoriesManager;
